36ddea266e2d0ad7d331168f600164192a910ff1
X.R <33901703+XaedroX009@users.noreply.github.com>
2021-04-05 11:53:20 -0400

    Delete keys.js

--- Server/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://Xaedrox:K7ZNbFFYD9pOjobT@cinetcluster-ack9z.mongodb.net/test?retryWrites=true&w=majority",
-  JWT_SECRET: "YD6EAciaehK&@#*&123auisdb348^#($yaushdk3k",
-};
